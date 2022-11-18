a6abcd187704c4d28de40058bec38cd1d8413923
IrisZong <77033184+IrisZong@users.noreply.github.com>
2022-11-18 13:01:14 -0500

    Add files via upload

---
+++ U2_mini.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+
+
+public class U2_mini {
+    
+    public static void main(String args[]) {
+
+        Scanner scan = new Scanner(System.in); 
+        System.out.println(" pls enter the other two side of the  triangle  ");
+        double i=scan.nextDouble();
+        double j=scan.nextDouble();
+    
+        double hy=sqrt((i*i+j*j));
+       
+    
+     System.out.println(" the hypotenuse is "+hy);
+           
+
+
+}
+
+}
